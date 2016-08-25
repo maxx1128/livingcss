e74470136593d30f96927d9fc0f852a7b9d23479
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2016-08-25 13:53:56 -0400

    Updated core styles and mobile nav display

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -5,6 +5,9 @@ var livingcss = require('../../index.js');
 livingcss('buttons.css', '../../styleguide.html', {
   preprocess: function(context, template, Handlebars) {
     context.title = 'My Awesome Style Guide';
+    context.logo_img = 'http://placehold.it/150x50';
+
+    context.menuText = 'Menu';
 
     // register a Handlebars partial
     Handlebars.registerPartial('myPartial', '{{name}}');
