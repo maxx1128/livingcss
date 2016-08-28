f8d4f48ff03c4c73e661fcd66bf0583e2aeaecc8
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2016-08-28 14:52:25 -0400

    Fine-tuned templates and Sass

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -4,10 +4,11 @@ livingcss('buttons.css', '../../styleguide.html', {
   preprocess: function(context, template, Handlebars) {
     context.globalStylesheets = ['../examples/basic/buttons.css'];
 
-    context.title = 'My Awesome Style Guide';
+    context.title = 'My Awesome StyleGuide';
     context.logo_img = 'http://placehold.it/150x50';
-
     context.menuText = 'Menu';
+
+    context.footerText = 'This is my awesome StyleGuide footer';
 
     // register a Handlebars partial
     Handlebars.registerPartial('myPartial', '{{name}}');
